02151ee97101eb5809bfdea1de78351801eeb0a2
LucaGiab714 <luca.giabbani@sts-sio-caen.info>
2023-04-12 12:50:29 +0200

    xqsd

--- front-next/src/component/Input.tsx
+++ front-next/src/component/Input.tsx
@@ -40,4 +40,44 @@ const PasswordInput: React.FC<PasswordInputProps> = ({ label, value, onChange })
     );
 }
 
+
+
+type  MinLengthInputProps = {
+    value:string;
+    label:string;
+    Onchange: (value:string) => void;
+}
+
+const MinLengthInput: React.FC<MinLengthInputProps> = ({ label, value, onChange }) => {
+    const [inputValue, setInputValue] = useState<string>("");
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        if (inputValue.length < props.minLength) {
+            alert(`La valeur doit contenir au moins ${props.minLength} caractères.`);
+        } else {
+            alert(`La valeur "${inputValue}" a été soumise.`);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                {props.labelText}
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    minLength={props.minLength}
+                />
+            </label>
+            <button type="submit">Soumettre</button>
+        </form>
+    );
+}
+export default MinLengthInput;
 export default PasswordInput;
